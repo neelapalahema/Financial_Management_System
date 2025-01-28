327a2b7139e6d2d58cd0a048f7830346bf851adf
neelapalahema <121088705+neelapalahema@users.noreply.github.com>
2025-01-28 11:38:54 +0530

    Add files via upload

---
+++ config/connectDb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const connectDb = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017');
+    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
+
+module.exports = connectDb;
